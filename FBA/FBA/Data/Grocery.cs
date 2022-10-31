3c8b0b26c6ad0856300b1cab6a531dccfcb13507
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-10-31 14:32:59 -0500

    finished forms and validations

--- FBA/FBA/Data/Grocery.cs
+++ FBA/FBA/Data/Grocery.cs
@@ -1,8 +1,14 @@
-﻿namespace FBA.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FBA.Data
 {
     public class Grocery
     {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters")]
         public string Name { get; set; }
+        [Required]
+        [Range(1,10000, ErrorMessage = "Valid price range is (1-10000)")]
         public float Price { get; set; }
     }
 }
