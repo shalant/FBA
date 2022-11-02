1a3b3f558ac342abac1efe45971f0249acfd3b5d
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-01 22:08:49 -0500

    started api portion

--- MVCLibrary/MVCLibrary/Controllers/BooksController.cs
+++ MVCLibrary/MVCLibrary/Controllers/BooksController.cs
@@ -54,7 +54,7 @@ namespace MVCLibrary.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BookID,Title,CallNumber")] Book book)
+        public async Task<IActionResult> Create([Bind("BookID,Title,CallNumber,Author")] Book book)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace MVCLibrary.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("BookID,Title,CallNumber")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("BookID,Title,CallNumber,Author")] Book book)
         {
             if (id != book.BookID)
             {
