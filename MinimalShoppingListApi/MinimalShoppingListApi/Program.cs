997429146ac1f10f933316e6c47df9c8a99ce104
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-02 10:59:19 -0500

    started invoice app

--- MinimalShoppingListApi/MinimalShoppingListApi/Program.cs
+++ MinimalShoppingListApi/MinimalShoppingListApi/Program.cs
@@ -12,6 +12,50 @@ var app = builder.Build();
 app.MapGet("/shoppinglist", async (ApiDbContext db) =>
     await db.Groceries.ToListAsync());
 
+app.MapGet("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+    var grocery = await db.Groceries.FindAsync(id);
+
+    return grocery !=null ? Results.Ok() : Results.NotFound();
+});
+
+app.MapPost("/shoppinglist", async (Grocery grocery, ApiDbContext db) =>
+{
+    db.Groceries.Add(grocery);
+
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/shoppinglist/{grocery.Id}", grocery);
+
+});
+
+app.MapDelete("/shoppinglist/{id}", async (int id, ApiDbContext db) =>
+{
+    var grocery = await db.Groceries.FindAsync(id);
+
+    if (grocery != null)
+    {
+        db.Groceries.Remove(grocery);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapPut("/shoppinglist/{id}", async (int id, Grocery grocery, ApiDbContext db) =>
+{
+    var groceryInDb = await db.Groceries.FindAsync(id);
+    if (groceryInDb != null)
+    {
+        groceryInDb.Name = grocery.Name;
+        groceryInDb.Purchased = grocery.Purchased;
+        await db.SaveChangesAsync();
+        return Results.Ok(groceryInDb);
+    }
+    return Results.NotFound();
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
